349190453693c634b83c8cd25499f3db31ac614c
DiegoA00 <leoancab@espol.edu.ec>
2025-05-29 16:05:29 -0500

    arreglos de código en clase student

--- codingstandards/Student.java
+++ codingstandards/Student.java
@@ -3,5 +3,27 @@ package codingstandards;
 import java.util.*;
 
 public class Student {
-    
+    String id;
+    String name;
+    ArrayList<Double> grades;
+    String pass = "unknown";
+    boolean honor;
+
+    public Student(String i, String n) {
+        id = i;
+        name = n;
+        grades = new ArrayList<>();
+    }
+
+    public void addGrade(Double g) {
+        grades.add(g);
+    }
+
+    public double average() {
+        double total = 0;
+        for (Double g : grades) {
+            total += g;
+        }
+        return total / grades.size();
+    }
 }
